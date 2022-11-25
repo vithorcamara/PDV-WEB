c1cb662966c0f0062dd118b9bb66dec0b4b578d0
vithor5 <vitor.camarapd@gmail.com>
2022-11-25 18:46:29 -0300

    Vendas 1.0

--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@ import {Router} from './router.js'
 
 const router = new Router()
 router.add('/', "/pages/home.html")
+router.add('/index.html', "/pages/home.html")
 router.add("/vendas", "/pages/vendas.html")
+router.add("/vendasclientes", "/pages/vendasclientes.html")
+router.add("/vendashistorico", "/pages/vendashistorico.html")
 router.add("/produtos", "/pages/produtos.html")
 router.add("/compras", "/pages/compras.html")
 router.add("/financeiro", "/pages/financeiro.html")
